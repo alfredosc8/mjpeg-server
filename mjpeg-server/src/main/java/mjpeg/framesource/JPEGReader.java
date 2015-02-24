bd6d36f2814ec5ab9592613ab9f2d3afb7a5c7e6
mmdw <mmdw631@gmail.com>
2015-02-24 22:01:55 +0400

    fix

--- mjpeg-server/src/main/java/mjpeg/framesource/JPEGReader.java
+++ mjpeg-server/src/main/java/mjpeg/framesource/JPEGReader.java
@@ -42,6 +42,8 @@ public class JPEGReader {
 						if (unsigned < 0xD0 || unsigned > 0xD7) {
 							if (unsigned == 0xD9) {
 								break mainLoop;
+							} else {
+								break;
 							}
 						}
 					}
